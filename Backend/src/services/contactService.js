b7818df1f0a0e3ecbcbfade74f9b2dc66aa0bfbe
Leo.Lamarre <leo.lamarre24@gmail.com>
2025-11-04 16:10:23 +0100

    FIX : changement d'un console.log dans contactService

--- Backend/src/services/contactService.js
+++ Backend/src/services/contactService.js
@@ -1,7 +1,6 @@
 const sendContactEmail = async (data) => {
     // Simuler un traitement de l'email
-    console.log(`Traitement d'un email pour l'objet : ${data.objet}`);
-    console.log(`De la part de : ${data.nom}`);
+    console.log(`Traitement d'un email de la part de : ${data.nom} / ${data.email}`);
 
     // Simuler un léger délai
     await new Promise((resolve) => setTimeout(resolve, 1000));
