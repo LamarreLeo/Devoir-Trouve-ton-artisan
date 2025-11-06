a9fa9519aa3cd96d2ea268f82a55447ce5a78ec5
Leo.Lamarre <leo.lamarre24@gmail.com>
2025-11-06 17:09:17 +0100

    Création du fichier de configuration Axios

---
+++ Frontend/src/main 2.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
+import './index.css';
+import App from "./App";
+import Home from "./pages/Home";
+import Search from "./pages/Search";
+import Artisan from "./pages/Artisan";
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <App />,
+        children: [
+            {
+                path: "/",
+                element: <Home />,
+            },
+            {
+                path: "/search",
+                element: <Search />,
+            },
+            {
+                path: "/artisans/:id",
+                element: <Artisan />,
+            },
+        ],
+    },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+    <React.StrictMode>
+        <RouterProvider router={router} />
+    </React.StrictMode>
+);
