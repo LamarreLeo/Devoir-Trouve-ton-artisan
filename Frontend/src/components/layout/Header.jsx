e0a4f6789d5e7714852f9c42ca1efbdf84ee1757
Leo.Lamarre <leo.lamarre24@gmail.com>
2025-11-07 15:00:21 +0100

    Styling du Header pour Desktop & Tablet

--- Frontend/src/components/layout/Header.jsx
+++ Frontend/src/components/layout/Header.jsx
@@ -1,6 +1,7 @@
 import { useEffect, useState } from "react";
 import { NavLink, Link, useNavigate } from "react-router-dom";
 import { getCategories } from "../../services/api";
+import { Search } from "lucide-react";
 
 function Header() {
     const [search, setSearch] = useState("");
@@ -32,37 +33,46 @@ function Header() {
     }, []);
 
     return (
-        <header className="flex items-center justify-between shadow-sm lg:h-[135px] ">
+        <header className="flex items-center justify-between shadow-sm h-[75px] md:h-[135px] lg:h-[135px] ">
             {/* Logo */}
             <Link to="/">
                 <img
                     src="src/assets/img/Logo.png"
                     alt="Logo de Trouve Ton Artisan !"
-                    className="w-[380px]"
+                    className="w-[200px] md:w-[350px] lg:w-[380px]"
                 />
             </Link>
 
             {/* Header Items */}
-            <div className="flex flex-col pr-10">
+            <div className="flex md:flex-col pr-10 h-full justify-center gap-5">
                 {/* Search Bar */}
-                <div className="flex items-center">
-                    <form onSubmit={handleSearch}>
+                <div className="hidden md:flex justify-center">
+                    <form
+                        onSubmit={handleSearch}
+                        className="border border-[#0074C5] rounded-md p-2 flex items-center"
+                    >
                         <input
                             type="text"
-                            placeholder="Rechercher"
+                            placeholder="Chercher un artisan"
                             value={search}
                             onChange={(e) => setSearch(e.target.value)}
+                            className="focus:outline-none w-[250px]"
                         />
-                        <button type="submit">Rechercher</button>
+                        <button type="submit">
+                            <Search className="w-5 h-5 text-[#0074C5]" />
+                        </button>
                     </form>
                 </div>
 
                 {/* Header Links */}
-                <nav>
+                <nav className="hidden md:flex justify-center">
                     <ul className="flex gap-5">
                         {categories.map((category) => (
                             <li key={category.nom}>
-                                <NavLink to={`/search?category=${category.nom}`}>
+                                <NavLink
+                                    to={`/search?category=${category.nom}`}
+                                    className="hover:underline"
+                                >
                                     {category.nom}
                                 </NavLink>
                             </li>
