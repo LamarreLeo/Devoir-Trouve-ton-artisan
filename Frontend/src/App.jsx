5541e6339979b3bb3cb04d45006b7e264ae7e8c0
Leo.Lamarre <leo.lamarre24@gmail.com>
2025-11-10 15:48:00 +0100

    Integration du footer

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,6 +1,7 @@
 import './App.css'
 import { Outlet } from "react-router-dom";
 import Header from "./components/layout/Header";
+import Footer from "./components/layout/Footer";
 
 export default function App() {
     return (
@@ -9,6 +10,7 @@ export default function App() {
             <main className="flex flex-col items-center justify-center w-full max-w-[1280px] mx-auto gap-20 mt-10 md:mt-20 mb-10 md:mb-20 lg:gap-30">
                 <Outlet />
             </main>
+            <Footer />
         </>
     );
 }
