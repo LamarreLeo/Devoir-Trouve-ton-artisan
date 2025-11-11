35617173b376768615f07a9508688e62d109cd7b
Leo.Lamarre <leo.lamarre24@gmail.com>
2025-11-11 02:29:50 +0100

    Ajout d'un titre et d'une description à la la page 404

--- Frontend/src/pages/NotFound.jsx
+++ Frontend/src/pages/NotFound.jsx
@@ -1,8 +1,17 @@
 import { Link } from "react-router-dom";
+import { Helmet } from "react-helmet-async";
 
 function NotFound() {
     return (
-        <div className="min-h-100 flex flex-col items-center justify-center p-6 gap-6 text-center">
+        <>
+            <Helmet>
+                <title>Erreur 404 - Trouve Ton Artisan !</title>
+                <meta
+                    name="description"
+                    content="Cette page n'existe pas ! Elle n'a jamais franchi la ligne d'arrivée."
+                />
+            </Helmet>
+            <div className="min-h-100 flex flex-col items-center justify-center p-6 gap-6 text-center">
             <h1 className="text-2xl md:text-4xl dark-blue">Erreur 404</h1>
             <p className="dark text-xs md:text-sm lg:text-base">
                 Cette page n'existe pas ! Elle n'a jamais franchi la ligne
@@ -13,6 +22,7 @@ function NotFound() {
             </Link>
             <img src="src/assets/img/ERREUR 404-VISU.jpg" alt="Erreur 404" />
         </div>
+        </>
     );
 }
 
