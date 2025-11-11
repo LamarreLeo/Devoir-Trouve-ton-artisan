45dc7d7867a628f2529950616a40a3c69cec213d
Leo.Lamarre <leo.lamarre24@gmail.com>
2025-11-11 02:29:09 +0100

    Ajout d'un titre et d'une description à la la page (Page en construction)

--- Frontend/src/pages/Construction.jsx
+++ Frontend/src/pages/Construction.jsx
@@ -1,13 +1,24 @@
+import { Helmet } from "react-helmet-async";
+
 function Construction() {
     return (
-        <div className="min-h-100 flex items-center justify-center p-6">
-            <div className="text-center">
-                <h1 className="text-2xl font-bold mb-4 dark-blue">
-                    Page en construction
-                </h1>
-                <p className="dark">Cette page sera bientôt disponible.</p>
+        <>
+            <Helmet>
+                <title>Page en construction - Trouve Ton Artisan !</title>
+                <meta
+                    name="description"
+                    content="Cette page sera bientôt disponible."
+                />
+            </Helmet>
+            <div className="min-h-100 flex items-center justify-center p-6">
+                <div className="text-center">
+                    <h1 className="text-2xl font-bold mb-4 dark-blue">
+                        Page en construction
+                    </h1>
+                    <p className="dark">Cette page sera bientôt disponible.</p>
+                </div>
             </div>
-        </div>
+        </>
     );
 }
 
